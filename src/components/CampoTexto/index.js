072e2032eb7a3928758d001f7cb314f4ecff8c37
manuelscl <manuelscardenaslopez@gmail.com>
2023-04-23 10:37:50 -0600

    Campos requeridos

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -5,7 +5,7 @@ const CampoTexto = (props) => {
     const placeholderModificado = `${props.placeholder}...`;
     return <div className="campo-texto">
         <label>{props.titulo.toUpperCase()}</label>
-        <input placeholder={placeholderModificado}/>
+        <input placeholder={placeholderModificado} required={props.required}/>
     </div>
 }
 
--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -6,16 +6,16 @@ import Boton from "../Boton";
 const Formulario = () => {
 
     const manejarEnvio = (e) => {
-        evento.preventDefault();
+        e.preventDefault();
         console.log("Manejar envio", e);
     }
 
     return <section className="formulario">
         <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
-            <CampoTexto titulo="Puesto" placeholder="Ingresa tu puesto"/>
-            <CampoTexto titulo="Foto" placeholder="Agregar enlace de foto"/>
+            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required />
+            <CampoTexto titulo="Puesto" placeholder="Ingresa tu puesto" required />
+            <CampoTexto titulo="Foto" placeholder="Agregar enlace de foto" required />
             <ListaOpciones />
             <Boton>
                 Crear colaborador
